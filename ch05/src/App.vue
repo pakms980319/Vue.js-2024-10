2ed1e672db98b83d1bf165749b7c7ac9195360a3
pakms980319 <pakms980319@gmail.com>
2024-12-08 00:17:56 +0900

    컴포넌트 생명주기 (2)

--- ch05/src/App.vue
+++ ch05/src/App.vue
@@ -1,12 +1,23 @@
 <script>
-  export default {
-    mounted() {
-      const domEl = this.$refs.domEl;
-      const text = domEl.innerHTML;  // this.$refs.domEl.innerText 와 같은 의미
-      console.log(text);
-    },
-  }
+import FirstChild3 from "@/components/FirstChild3.vue";
+export default {
+  components: {
+    FirstChild3,
+  },
+  mounted() {
+    const childNum = this.$refs.child.num;
+    const childDoubleNum = this.$refs.child.doubleNum;
+    console.log(`메서드 호출 전: ${childNum}`); // 5 출력
+    console.log(`메서드 호출 전: ${childDoubleNum}`); // 10 출력
+    // DOM이 마운트되고 1초 뒤에 TheChild 컴포넌트의 increment() 메서드 호출
+    setTimeout(() => {
+      this.$refs.child.increment();
+      console.log(`메서드 호출 전: ${this.$refs.child.num}`); // 6 출력
+      console.log(`메서드 호출 전: ${this.$refs.child.doubleNum}`); // 12 출력
+    }, 1000);
+  },
+};
 </script>
 <template>
-  <h1 ref="domEl">h1 태그</h1>
+  <FirstChild3 ref="child" />
 </template>
