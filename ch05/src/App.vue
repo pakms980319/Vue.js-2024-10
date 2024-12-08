ede618ce0bae305a0bca896915260a638beff29e
pakms980319 <pakms980319@gmail.com>
2024-12-09 08:43:57 +0900

    컴포넌트의 데이터 효율적으로 다루기 - 컴포넌트의 데이터 공유하기 (OptionsAPI-provide)

--- ch05/src/App.vue
+++ ch05/src/App.vue
@@ -1,20 +1,21 @@
 <script>
-  import UserProfile3 from './components/UserProfile3.vue';
+  import FirstChild4 from './components/FirstChild4.vue';
   export default {
     components: {
-      UserProfile3,
+      FirstChild4,
     },
-    methods: {
-      printHello(name, age) {
-        alert(`안녕하세요! 저는 ${name}이고, 나이는 ${age}살 입니다.`);
-      },
+    provide() {
+      return {
+        message: this.message,
+      };
+    },
+    data() {
+      return {
+        message: 'Hello, Vue JS!',
+      };
     },
   }
 </script>
 <template>
-  <!--
-    <UserProfile3 @print-hello="(name, age) => printHello(name, age)" />
-  -->
-  <!-- 비구조화 할당으로 인자의 값을 받아 처리 - 객체 -->
-   <UserProfile3 @print-hello="({name, age}) => printHello(name, age)" />
+  <FirstChild4 />
 </template>
