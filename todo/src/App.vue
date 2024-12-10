948b1024dba0753d5cea348975a4fa4a7119f251
박민석 <pakms980319@gmail.com>
2024-12-10 11:58:01 +0900

    6장 todo app 실습

---
+++ todo/src/App.vue
@@ -0,0 +1,58 @@
+<script>
+  import TodoHeader from './components/TodoHeader.vue';
+  import TodoList from './components/TodoList.vue';
+  import TodoInput from './components/TodoInput.vue';
+
+  export default {
+    components: {
+      TodoHeader,
+      TodoList,
+      TodoInput,
+    },
+    data() {
+      return {
+        todo: [],
+        current: 'all',
+      };
+    },
+    methods: {
+      addTodo(inputMsg) {
+        const item = {
+          id: Math.random(),  // 고유한 값
+          msg: inputMsg,  // 할 일 텍스트
+          computed: false,  // 할 일 완료 여부
+        };
+        this.todo.push(item);
+      },
+      updateTab(tab) {
+        this.current = tab;
+      },
+      deleteTodo(id) {
+        this.todo = this.todo.filter((v) => v.id !== id);
+      },
+      updateTodo(id) {
+        this.todo = this.todo.map((v) => v.id === id ? { ...v, completed: !v.completed } : v);
+      }
+    },
+    computed: {
+      computedTodo() {
+        if (this.current === 'all') {
+          return this.todo;
+        } else {
+          return this.todo.filter((v) => v.completed);
+        }
+      },
+    }
+  }
+</script>
+
+<template>
+  <div class="todo">
+    <TodoHeader :current="current" @update-tab="updateTab" />
+    <TodoList 
+        :computed-todo="computedTodo"
+        @delete-todo="deleteTodo"
+        @update-todo="updateTodo"/>
+    <TodoInput @add-todo="addTodo" />
+  </div>
+</template>
